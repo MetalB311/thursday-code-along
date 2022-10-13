1fa442a3d447188346ab664b8db8dae86d0b8d10
MetalB <metalb311@gmail.com>
2022-10-13 13:56:34 -0600

    adds get all chars function

--- main.js
+++ main.js
@@ -44,6 +44,24 @@ function getOneChar(event) {
   }) 
 }
 
+function getOldChars(event) {
+event.preventDefault()
+
+clearCharacters()
+
+const age = ageInput.value
+
+  axios.get(baseURL + '/character/' + '?age=' + age)
+  .then((response) => {
+    for (i=0; i < response.data.length; i++) {
+      createCharacterCard(response.data[i])
+    }
+  })
+  .catch(() => {
+    console.log('got an error: ' + err)
+  })
+}
+
 
 function createCharacterCard(char) {
   let charCard = document.createElement('div')
@@ -64,7 +82,7 @@ function clearCharacters() {
 }
 
 getAllBtn.addEventListener('click', getAllChars)
-
+ageForm.addEventListener('submit', getOldChars)
 for (let i=0; i<charBtns.length; i++){
   charBtns[i].addEventListener('click', getOneChar)
 }
